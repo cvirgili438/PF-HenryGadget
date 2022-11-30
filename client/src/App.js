b74c25bde4c5b25051710ded838d6ff9c937db0b
Nicolas Stabilini <stabilini@gmail.com>
2022-11-30 17:34:30 -0300

    ruta /test para probar componentes/

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import LandingPage from './Component/LandingPage/LandingPage.jsx';
+import Product from './Component/Product/Product.jsx';
 
 function App() {
   return (
     <BrowserRouter >
     <div className="App">
       <Switch >
-      <Route exact path='/' component={LandingPage} />
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/test'>
+          <Product />
+        </Route>
       </Switch>      
     </div>
     </BrowserRouter>
