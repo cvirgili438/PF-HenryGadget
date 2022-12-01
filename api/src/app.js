6f662d376e6daab47d3d2af52fe2e0e256594bdb
FerB <ferb@e.email>
2022-12-01 11:21:30 -0300

    Se agrega ruta products.

--- api/src/app.js
+++ api/src/app.js
@@ -23,6 +23,7 @@ server.use((req, res, next) => {
 });
 
 // AQUI VAN LOS MIDDLEWIRES
+server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
