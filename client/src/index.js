3d89bf2ca4a2663d988482f3efdec822237f2ab9
cvirgili438 <100769556+cvirgili438@users.noreply.github.com>
2022-12-01 12:01:37 -0300

    Se cambia de lugar el ACTION REDUCER Y STORE a la carpeta redux, se genera la funcion product by name en action y estado nuevo de productos filtrados en el reducer

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'
-import {store} from './Store/index.js'
+import {store} from './Redux/Store/index'
 
 ReactDOM.render(
   <Provider store={store}>
