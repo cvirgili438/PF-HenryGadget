0d70b7d977257f0ac656b7432dc9bb7623b86e4a
cvirgili438 <100769556+cvirgili438@users.noreply.github.com>
2022-12-01 22:56:30 -0300

    correccion errores menores

--- client/src/Redux/Action/index.js
+++ client/src/Redux/Action/index.js
@@ -32,9 +32,9 @@ export const getProductsByQuery = (query)=> async (dispatch)=>{ // recibo un obj
     let arrayObjeto = Object.entries(query) // pongo el objeto en formato array tipo [[Propiedad1,valor propiedad1],[Propiedad2,Valor2]] etc
     let stringToUrl = ''
     arrayObjeto.map((el)=>{
-        if(arrayObjeto.length -1 == arrayObjeto.indexOf(el)) // si el index es el ultimo, no agrega & sino si
-        {stringToUrl = stringToUrl.concat(`${el[0]}=${el[1]}`)} 
-        else {{stringToUrl = stringToUrl.concat(`${el[0]}=${el[1]}&`)}}
+        if(arrayObjeto.length -1 === arrayObjeto.indexOf(el)) // si el index es el ultimo, no agrega & sino si
+        {return stringToUrl = stringToUrl.concat(`${el[0]}=${el[1]}`)} 
+        else {return stringToUrl = stringToUrl.concat(`${el[0]}=${el[1]}&`)}
     })
     try{
         
