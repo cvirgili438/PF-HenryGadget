26fd6431e386148f26142f6959c83feb0993a11c
Nicolas Stabilini <stabilini@gmail.com>
2022-12-02 00:45:53 -0300

    checkbox y product admin (en proceso)

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import LandingPage from './Component/LandingPage/LandingPage.jsx';
+import ProductCRUD from './Component/ProductCRUD/ProductCRUD';
 
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
       <div className="App">
         <Switch >
           <Route exact path='/'>
-            <LandingPage />
+            <ProductCRUD />
           </Route>
         </Switch>      
       </div>
