13fdfaec750334acd08f5761bc1907c91fe8003f
FerB <ferb@e.email>
2022-12-02 08:01:22 -0300

    Se crean relaciones.

--- api/src/db.js
+++ api/src/db.js
@@ -39,10 +39,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-
+const { Product, Review, User } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+Product.belongsToMany(User, { through: Review});
+User.belongsToMany(Product, { through: Review});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
