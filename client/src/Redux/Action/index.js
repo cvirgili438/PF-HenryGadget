e160eef7a5729b2f83505b786e16cd6e269d0811
cvirgili438 <100769556+cvirgili438@users.noreply.github.com>
2022-12-01 22:53:41 -0300

    Se Agrego funciones para buscar por query y por nombre

--- client/src/Redux/Action/index.js
+++ client/src/Redux/Action/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const GET_PRODUCTS = 'GET_PRODUCTS'
 export const FILTER_BY_NAME = 'FILTER_BY_NAME'
+export const GET_PRODUCTS_BY_QUERY= 'GET_PRODUCTS_BY_QUERY'
 
 
 export const getAllProducts= ()=> async (dispatch)=>{   
@@ -25,4 +26,27 @@ export const getProductByName = (name)=> async (dispatch)=>{
         })
     }
     catch(er){console.log(er)}
+}
+
+export const getProductsByQuery = (query)=> async (dispatch)=>{ // recibo un objeto query
+    let arrayObjeto = Object.entries(query) // pongo el objeto en formato array tipo [[Propiedad1,valor propiedad1],[Propiedad2,Valor2]] etc
+    let stringToUrl = ''
+    arrayObjeto.map((el)=>{
+        if(arrayObjeto.length -1 == arrayObjeto.indexOf(el)) // si el index es el ultimo, no agrega & sino si
+        {stringToUrl = stringToUrl.concat(`${el[0]}=${el[1]}`)} 
+        else {{stringToUrl = stringToUrl.concat(`${el[0]}=${el[1]}&`)}}
+    })
+    try{
+        
+        let json = await  axios(`http://localhost3001/Products?${stringToUrl}`)
+        return dispatch({
+            type:GET_PRODUCTS_BY_QUERY,
+            payload:json.data
+        })
+    }
+    catch(er){console.log(er.messege)}
+}
+
+export const paginated = (value)=>(dispatch)=>{
+    // falta ponerse deacuerdo sobre cuatno en cuanto va a ir el paginado
 }
