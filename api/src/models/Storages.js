1d22167a39abb445056f4c6216579b696005e151
FerB <ferb@e.email>
2022-12-03 09:40:07 -0300

    Se cambia nombre de campo por otro más adecuado.

--- api/src/models/Storages.js
+++ api/src/models/Storages.js
@@ -9,7 +9,7 @@ module.exports = (sequelize) => {
       defaultValue: DataTypes.UUIDV4,
       primaryKey:true,     
     },
-    name: {
+    cant: {
       type: DataTypes.STRING,
       allowNull: false,
     },
