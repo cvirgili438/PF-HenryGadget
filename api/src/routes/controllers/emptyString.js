a8faa77c297fa30b341e3f14a27dd51b4a955709
Moises David Plata Florez <97771089+Moiso2001@users.noreply.github.com>
2022-12-03 21:49:44 -0500

    Delete emptyString.js
    Delete emptyString.js

--- api/src/routes/controllers/emptyString.js
+++
@@ -1,4 +0,0 @@
-module.exports = (string) => {
-    return    // Tomamos el string y convertimos un array con cada caracter, luego filtramos todos 
-}                                                           // los caracteres ' ' si queda alguna letra significa que no es un string vacio si todo
-                                                           // se filtro pues era un string vacio.
