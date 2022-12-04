3f3ec3f80807814c40996be275263deddee6b8e9
FerB <ferb@e.email>
2022-12-04 06:20:10 -0300

    Se establece igual formato para todas las respuestas en review.

--- api/src/routes/reviews.js
+++ api/src/routes/reviews.js
@@ -11,8 +11,8 @@ router.get('/', async (req,res)=> {                                             
             const result = await Review.findAll();  
             
             result.length === 0                                                         // Si no hay reviews disponibles devolvera un array vacio, se valida y muestra msg apropiado, misma logica aplica para todos los casos
-            ? res.status(404).json({msg: "There are no reviews available"})
-            : res.status(200).json(result);
+            ? res.status(404).json({err: "There are no reviews available."})
+            : res.status(200).json({msg: 'Reviews obtained successfully.', result: result});
             return
         }
 
@@ -20,15 +20,15 @@ router.get('/', async (req,res)=> {                                             
             const result = await Review.findAll({where:{productId: idProduct}});
             
             result.length === 0
-            ? res.status(404).json({msg: "There are no reviews available for this product or the product doesn't exist"})
-            : res.status(200).json(result);
+            ? res.status(404).json({err: "There are no reviews available for this product or the product doesn't exist."})
+            : res.status(200).json({msg: 'Reviews obtained successfully.', result: result});
             return
         }
 
         const result = await Review.findAll({where: {userId: idUser}});                 // Ya la ultima situacion posible es el idUser, en ese caso devolver reviews filtradas por usuario.
         result.length === 0
-        ? res.status(404).json({msg: "There are no reviews available for this user or the user doesn't exist"})
-        : res.status(200).json(result);
+        ? res.status(404).json({err: "There are no reviews available for this user or the user doesn't exist."})
+        : res.status(200).json({msg: 'Reviews obtained successfully.', result: result});
     } catch (error) {
         res.status(400).json({err: error.message});
     }
@@ -38,9 +38,9 @@ router.post('/', async (req,res) => {                                           
     const {idProduct, idUser, reviewData} = req.body;                                           // Information recibida por body, id de usuario y product y un objeto de review, que tendra *score y comment los nombres de las propiedades de reviewData deben ser extrictamente esos
     const reviewDataValidate = reviewData || false;                                             // Validacion en caso de que reviewData sea null, evitar que rompa el servidor
 
-    if(!idProduct || !idUser) return res.status(400).json({err: 'Missing data'});               // Si falta algun id devuelve un error.
-    if(!reviewDataValidate) return res.status(400).json({err: 'Review data is missing'});       // Revisa que si hayan pasado ReviewData
-    if(!reviewDataValidate.score) return res.status(400).json({err: 'Review score is missing'});// Revisa que reviewData tenga la propiedad score, comments es opcional
+    if(!idProduct || !idUser) return res.status(400).json({err: 'Missing data.'});               // Si falta algun id devuelve un error.
+    if(!reviewDataValidate) return res.status(400).json({err: 'Review data is missing.'});       // Revisa que si hayan pasado ReviewData
+    if(!reviewDataValidate.score) return res.status(400).json({err: 'Review score is missing.'});// Revisa que reviewData tenga la propiedad score, comments es opcional
     
     try {
         const product = await Product.findByPk(idProduct);                                       // Encuentra el product por ID
@@ -50,9 +50,9 @@ router.post('/', async (req,res) => {                                           
         await user.addReview(review);                                                            // Linkeamos tanto en usuario y product la nueva review
         await product.addReview(review);
 
-        res.status(201).json({msg: 'review created succesfully', review: review});
+        res.status(201).json({msg: 'Review created succesfully.', result: review});
     } catch (error) {
-        res.status(404).json({err: "user or product doesn't exist"});
+        res.status(404).json({err: error.message});
     }
 })
 
@@ -63,12 +63,12 @@ router.delete('/admin/:idReview', async (req,res) => {                          
         const reviewToDelete = await Review.findByPk(idReview);                                                     // Buscamos la review por id para verificar luego
         if(idReview && reviewToDelete){                                                                             // Validamos que nos hayan pasado tanto id por params y que si exista en la db
             await Review.destroy({where: {id: idReview}})                                                           // Si si existe eliminamos y devolvemos mensaje apropiado
-            res.status(200).json({msg: 'Review deleted correctly', idReview})
+            res.status(200).json({msg: 'Review deleted correctly.', result: idReview})
             return;
         }
-        res.status(400).json({msg: `Review with id: ${idReview} was already deleted or doesn't exist`});            // En caso de que falle la validacion, devolveremos mensaje apropiado
+        res.status(400).json({err: `Review with id: ${idReview} was already deleted or doesn't exist.`});            // En caso de que falle la validacion, devolveremos mensaje apropiado
     } catch (error) {
-        res.status(400).json({msg: `Review with id: ${idReview} was already deleted or doesn't exist`, err: error})
+        res.status(400).json({err: error})
     }
 })
 
@@ -77,17 +77,17 @@ router.put('/:idReview', async (req,res) => {                                   
     const {reviewData} = req.body;
     const reviewDataValidate = reviewData || false;                                                                             // pequeña validacion para evitar que null no nos rompa el codigo
 
-    if(!idReview) return res.status(400).json({err: 'Review id is missing'});                                                   // Validaciones en caso de que algo falte
-    if(!reviewDataValidate.score && !reviewDataValidate.comment) return res.status(400).json({err: 'Review data is missing'});  
+    if(!idReview) return res.status(400).json({err: 'Review id is missing.'});                                                   // Validaciones en caso de que algo falte
+    if(!reviewDataValidate.score && !reviewDataValidate.comment) return res.status(400).json({err: 'Review data is missing.'});  
     
     try { 
         const review = await Review.findByPk(idReview);
         if(!review){
-            res.status(404).json({err: `Review with id: ${idReview} doesn't exist`});
+            res.status(404).json({err: `Review with id: ${idReview} doesn't exist.`});
             return;
         }
         const reviewUpdated = await Review.update(reviewData, {where: {id: idReview}});                                         // Se actualiza el comment
-        res.status(200).json({msg: `Review with id: ${idReview} was updated`, reviewUpdated})
+        res.status(200).json({msg: `Review with id: ${idReview} was updated`, result: reviewUpdated})
     } catch (error) {
         res.status(400).json({err: error})
     }
