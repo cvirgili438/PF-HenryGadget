cb6d64720b4a62347dc2cd8e3992964b94a8a981
FerB <ferb@e.email>
2022-12-04 11:39:17 -0300

    Se agrega array de Rams.

---
+++ api/src/controller/objectToAdd/ram.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        size: '2GB',
+        description: 'Alguna descripcion de la cantiad de RAM.'
+    },
+    {
+        size: '4GB',
+        description: 'Alguna descripcion de la cantiad de RAM.'
+    },
+    {
+        size: '8GB',
+        description: 'Alguna descripcion de la cantiad de RAM.'
+    },
+];
