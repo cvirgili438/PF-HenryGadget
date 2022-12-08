7ea649754842acf55412b69413223d5cf20ff4cc
FerB <ferb@e.email>
2022-12-08 18:30:08 -0300

    Se quita opción unique de name.

--- api/src/models/Products.js
+++ api/src/models/Products.js
@@ -12,7 +12,6 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
     },
     price: {
       type: DataTypes.FLOAT,
@@ -46,7 +45,6 @@ module.exports = (sequelize) => {
       type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false,
     },
-
   },
     {
       timestamps: true,
