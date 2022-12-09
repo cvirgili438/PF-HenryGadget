d13e2129f7cd253b75da539da3af370c82134cce
mijail pulgar <mijail86@gmail.com>
2022-12-09 08:31:51 -0500

    modificacion de middleware

--- api/src/routes/middleware/index.js
+++ api/src/routes/middleware/index.js
@@ -1,26 +1,30 @@
-const axios = require('axios')
 const admin = require('../config/firebase-config');
+const { User } = require('../../db.js')
 
 async function decodeToken(req, res, next) {
 	let token = undefined
 	if (req.headers.authorization) {
 		token = req.headers.authorization.split(' ')[1];
 	}
+	console.log(token)
 	try {
 		const decodeValue = await admin.auth().verifyIdToken(token);
+		console.log(decodeValue)
 		let uid = decodeValue.uid;
-		const res = await axios.get(`http://localhost:3001/users/${uid}`);
-		let rol = res.data.result.rol;
+		const user = await User.findOne({where: {uid}});
+		let rol = user.rol
 
 		if (decodeValue && rol === 'admin') {
 			req.user = decodeValue;
 			return next();
 		}
-		return res.json({ message: 'Un authorize' });
+		return res.json({ message: 'Not authorized' });
 	} catch (e) {
-		return res.json({ message: 'What are you trying to do my friend? You are not worthy for this' });
+		return res.json({ message: 'Not authorized' });
 	}
 }
+
+
 
 
 module.exports = decodeToken;
