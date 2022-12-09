9606fa135d4c1cac755135e454f4c0f8c8fe5ff5
FerB <ferb@e.email>
2022-12-09 19:20:44 -0300

    Se crea variable de entorno DB_NAME, nombre de la DB.

--- api/src/db.js
+++ api/src/db.js
@@ -3,12 +3,16 @@ const { Sequelize, Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/HenryGadget`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  // Para DB deploy. Suelen pedir conexión ssl.
+  // dialectOptions: {
+  //   ssl: true
+  // },
   define: {
     freezeTableName: true // Para que no le cambie el nombre a todas las tablas
   }
