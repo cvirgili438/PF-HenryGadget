0f75ba35237028a3145c98f2abd61c51addfc130
mijail pulgar <mijail86@gmail.com>
2022-12-10 17:58:00 -0500

    ruta para el logueo

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -61,7 +61,6 @@ router.post('/log', async (req, res) => {
     let token = undefined
 	if (req.headers.authorization) {
 		token = req.headers.authorization.split(' ')[1];
-		console.log(token)
 	}
     try {
         const decodeValue = await admin.auth().verifyIdToken(token);
