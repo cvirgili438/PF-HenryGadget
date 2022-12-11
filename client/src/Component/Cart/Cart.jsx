524acc2e0134ee8fad2399b9adbaa87f4c1bcd1d
cvirgili438 <100769556+cvirgili438@users.noreply.github.com>
2022-12-11 15:28:02 -0300

    falta cuando usercart leng > 0 y localCart lengt === 0

--- client/src/Component/Cart/Cart.jsx
+++ client/src/Component/Cart/Cart.jsx
@@ -43,6 +43,9 @@ const Cart = () => {
             dispatch(setLocalCart(storage))
             dispatch(setUserCart(storage,user.uid))
           }
+          if(userCart.length > 0 && localCart.length === 0 ){
+            
+          }
 
         }
         },[user])
