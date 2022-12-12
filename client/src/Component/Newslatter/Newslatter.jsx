74dde3ba9a7fff89340a53b66722ac79bb40a446
Leonardo Meza <leoariel17@gmail.com>
2022-12-12 11:54:50 -0300

    Modulito de Newslatter con miu. Falta la logica

--- client/src/Component/Newslatter/Newslatter.jsx
+++ client/src/Component/Newslatter/Newslatter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './Newslatter.module.css'
 import { Box, IconButton, Stack, TextField } from "@mui/material";
-import { EmailIcon } from '@mui/icons-material';
+import EmailIcon from '@mui/icons-material/Email';
 import SendIcon from '@mui/icons-material/Send';
 
 
@@ -13,22 +13,30 @@ const Newslatter = () => {
             <Box  sx={{
                 p:6,
                 bgcolor: 'rgb(244, 244, 244)',
-                height: 75
+                height: 125,
+                
+                
             }}>
-                <Stack direction={'row'}>
-                    <div>
+                <Stack direction={'row'} sx={{alignItems: 'center'}}>
+                    <Stack direction={'row'} sx={{
+                        alignItems: 'center',
+                        pr:5
+                        
+                    }}>
                         <EmailIcon />
-                        <h1>Newslatter</h1>
-                    </div>
-                    {/* <div>
-                        <p>Receive information about our products and promotions directly to your email.</p>
-                    </div>
+                        <h2>Newslatter</h2>
+                    </Stack>
+                    <Box sx={{
+                        alignItems: 'center'
+                    }}>
+                        <p className={`${styles.p}`}>Receive information about our products and promotions directly to your email.</p>
+                    </Box>
                     <div>
                         <TextField id="standard-basic" label="Email" variant="standard"></TextField>
                         <IconButton color="primary">
                             <SendIcon />
                         </IconButton>
-                    </div> */}
+                    </div>
                 </Stack>
             </Box>
         </div>
