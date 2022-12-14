c4340521830b16e07442ba09577267426df3ef1b
FerB <ferb@e.email>
2022-12-14 10:41:19 -0300

    Se agrega Footer en las rutas.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import NavBar from './Component/NavBar/NavBar.jsx';
 import LandingPage from './Component/LandingPage/LandingPage.jsx';
 import Detail from './Component/Detail/Detail';
+import Footer from './Component/Footer/Footer.jsx';
 import Page404 from './Component/Page404/Page404';
 
 const App = () => {
@@ -17,11 +18,13 @@ const App = () => {
           <Route exact path='/'>
             <NavBar />
             <Route exact path='/' component={LandingPage} />
+            <Footer />
           </Route>
 
           <Route path='/product'>
             <NavBar />
             <Route path="/product/:id" component={Detail} />
+            <Footer />
           </Route>
 
           <Route path="*">
