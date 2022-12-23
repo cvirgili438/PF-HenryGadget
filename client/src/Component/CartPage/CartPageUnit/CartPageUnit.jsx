8a7305b96e35398996696e0cfb79450998eb1b77
mijail pulgar <mijail86@gmail.com>
2022-12-23 14:34:17 -0500

    se agrega link para ir al detalle y se corrigue un bug

--- client/src/Component/CartPage/CartPageUnit/CartPageUnit.jsx
+++ client/src/Component/CartPage/CartPageUnit/CartPageUnit.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import styles from './CartPageUnit.module.css'
 
 import Button from '@mui/material/Button';
@@ -21,21 +22,20 @@ const CartPageUnit = (props) => {
     let [quantityl, setQuantity] = useState(props.item.quantity)
     let [remove, setRemove] = useState(false)
     let [checkStock, setCheckStock] = useState(false)
-    console.log(props.item.stock)
 
     useEffect(async () => {
         if (remove) {
             deleteProductCart(props.item.idProduct, props.user && props.user.uid, 0)
+            props.setLocalCart(await getAllCart(user && user.uid));
         }else{
+            setCheckStock(quantityl > props.item.stock)
             updateProductCart(props.item.idProduct, props.user && props.user.uid, quantityl)
         }
         
         props.setLocalCart(await getAllCart(user && user.uid));
     }, [quantityl, remove])
 
-    useEffect(() => {
-        setCheckStock(quantityl > props.item.stock)
-    }, [quantityl])
+   
 
     let handleCount = (e) => {
         if (e.target.id === 'minus') {
@@ -58,66 +58,64 @@ const CartPageUnit = (props) => {
   return (
     <Box key={props.item.idProduct} className={styles.subcontainer1} >
         <Box>
-        <img src={props.item.img} style={{width: "80px", margin: "20px"}} alt=""/>
+            <Link to={`/product/${ props.item.idProduct }`}>
+                <img className={styles.img} src={props.item.img}  alt=""/>
+            </Link>
         </Box>
         <Box>
-        <Grid 
-            container 
-            justifyContent="space-between" 
-        >
-            <Grid item xs={6}>
-            <Box>
-                <Typography variant='h6'>{props.item.name}</Typography>
-                <Typography variant='body1'>Model: {props.item.model}</Typography>
-            </Box>
+            <Grid 
+                container 
+                justifyContent="space-between" 
+            >
+                <Grid item xs={6}>
+                <Box>
+                    <Typography variant='h6'>{props.item.name}</Typography>
+                    <Typography variant='body1'>Model: {props.item.model}</Typography>
+                </Box>
+                </Grid>
+
+                <Grid item  xs={6}>
+                <Box>
+                    <Typography variant='h6'>Total unitario: ${props.item.price} </Typography>
+                    <Typography variant='h6'>Total parcial: ${props.item.price * quantityl} </Typography>
+                </Box>
+                </Grid>
             </Grid>
 
-            <Grid item  xs={6}>
             <Box>
-                <Typography variant='h6'>Total unitario: ${props.item.price} </Typography>
-                <Typography variant='h6'>Total parcial: ${props.item.price * quantityl} </Typography>
-            </Box>
-            </Grid>
-        </Grid>
-
-        <Box 
-        // sx={{display: "flex", justifyContent: "space-around", width: "50vw", marginTop: "20px"}}
-        >
-        
-        <Box
-            sx={{display: "flex", justifyContent: "space-around", width: "50vw", marginTop: "20px"}}
-        >
-            <Box>
-                <Button onClick={e => handleCount(e)} id="minus" className={`${styles.btn_minus}`}>
-                    <RemoveIcon id="minus" />
-                </Button>
-                <Input  
-                type='number'
-                sx={{width: "40px", border: "1px solid gray"}}
-                value={quantityl}
-                onChange={e => handlerChange(e)}  />
-                <Button onClick={e => handleCount(e)} id="plus" className={`${styles.btn_plus}`}>
-                    <AddIcon id="plus" ></AddIcon>
-                </Button>                            
-            </Box>
-
-            <Box>
-            <Button onClick={e => handlerDelete(e)} aria-label="delete">
-                <DeleteForeverIcon />
-            </Button>
-            </Box>
-
-        </Box>
-            <Box>
-                {checkStock && <Alert xs={{ width: 100 }} sx={{marginTop: "10px"}}
-                    variant="outlined" severity="error">
-                    There is not enough stock!
-                </Alert>}
-            </Box>
             
-            
-            
-        </Box>
+                <Box
+                    sx={{display: "flex", justifyContent: "space-around", width: "50vw", marginTop: "20px"}}
+                >
+                    <Box>
+                        <Button onClick={e => handleCount(e)} id="minus" className={`${styles.btn_minus}`}>
+                            <RemoveIcon id="minus" />
+                        </Button>
+                        <Input  
+                        type='number'
+                        sx={{width: "40px", border: "1px solid gray"}}
+                        value={quantityl}
+                        onChange={e => handlerChange(e)}  />
+                        <Button onClick={e => handleCount(e)} id="plus" className={`${styles.btn_plus}`}>
+                            <AddIcon id="plus" ></AddIcon>
+                        </Button>                            
+                    </Box>
+
+                    <Box>
+                        <Button onClick={e => handlerDelete(e)} aria-label="delete">
+                            <DeleteForeverIcon />
+                        </Button>
+                    </Box>
+
+                </Box>
+                <Box>
+                    {checkStock && <Alert xs={{ width: 100 }} sx={{marginTop: "10px"}}
+                        variant="outlined" severity="error">
+                        There is not enough stock!
+                    </Alert>}
+                </Box>
+                    
+            </Box>
         </Box>
     </Box>
   )
