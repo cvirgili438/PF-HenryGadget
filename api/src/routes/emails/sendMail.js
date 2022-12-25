67655c2b296881368d249d1175a59540ca14554b
FerB <ferb@e.email>
2022-12-25 20:04:55 -0300

    Se crea primer acercamiento de /newsletter/sendmail.

--- api/src/routes/emails/sendMail.js
+++ api/src/routes/emails/sendMail.js
@@ -18,9 +18,34 @@ const { addEmail,
     getContactByEmail,
     getListID,
     addContactToList,
-    deleteContactFromList } = require('./controlers/controllersNewsletter.js');
+    deleteContactFromList,
+    getContactsFromList } = require('./controlers/controllersNewsletter.js');
 
 router.post('/', async (req, res) => { // POST /newsletter/sendmail
+    const { html, subject } = req.body;
+    if (!html)
+        return res.status(400).json({ err: 'Html parameter missing.' });
+    if (!subject)
+        return res.status(400).json({ err: 'Subject parameter missing.' });
+
+    try {
+        const listId = await getListID(NAME_NEWSLETTER);
+        const listConstacs = await getContactsFromList(listId);
+ 
+        const msg = {
+            personalizations: listConstacs,
+            from: EMAIL_FROM_NEWSLETTER,
+            subject: subject,
+            html: html
+        }
+        await sgMail.send(msg);
+
+        res.json({ msg: 'Email send.' });
+    }
+    catch (error) {
+        res.status(400).json({ err: 'Error to send mail.', error });
+    }
+
 });
 
 module.exports = router;
