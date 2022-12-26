ebbab6e6f084279ab023ad929bd16e094676a6a4
ferb0 <ferb@e.email>
2022-12-26 20:41:21 -0300

    Se termina las rutas de subscribe, confirm y unsubscribe. Falta sendmail

--- api/src/routes/emails/newsletter.js
+++ api/src/routes/emails/newsletter.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { sgMail, sgClient } = require('../config/sendgrid-config.js');
+const { Newsletter } = require('../../db.js');
+
+const { sgMail } = require('../config/sendgrid-config.js');
 
 const {
     EMAIL_FROM_NEWSLETTER,
@@ -14,21 +16,15 @@ const {
     HTML_UNSUBSCRIBE
 } = require('./constants/dataToSendMail.js');
 
-const { addEmail,
-    getContactByEmail,
-    getListID,
-    addContactToList,
-    deleteContactFromList } = require('./controlers/controllersNewsletter.js');
-
 router.post('/subscribe', async (req, res) => {
     const { email } = req.body;
     if (!email)
         return res.status(400).json({ err: 'Email parameter missing.' });
 
     // Número que solo servirá de verificación.
-    const confirmNumber = Math.ceil(Math.random() * 1000000000000);
+    const code = Math.ceil(Math.random() * 1000000000000);
     // Parámetros para la URL de confirmación.
-    const params = new URLSearchParams({ confirmNumber, email });
+    const params = new URLSearchParams({ code, email });
     // Página del Front.
     const confirmationURL = req.headers.origin + '/NewsletterConfirm?' + params;
     // Datos para el mail que enviará la API SendGrid.
@@ -40,9 +36,8 @@ router.post('/subscribe', async (req, res) => {
     }
 
     try {
-        // Se agrega correo a la lista global de contactos en SendGril
-        // (temporal hasta que confirme código).
-        await addEmail(email, confirmNumber);
+        // Se agrega correo a la tabla newsletter
+        await Newsletter.create({ email, code });
         // Se envía datos a SendGrid para que envíe el correo al destino.
         await sgMail.send(msg);
         res.json({ msg: `${email}, added successfully.` });
@@ -53,34 +48,35 @@ router.post('/subscribe', async (req, res) => {
 });
 
 router.post('/confirm', async (req, res) => {
-    const { email, confirmNumber } = req.body;
+    const { email, code } = req.body;
     if (!email)
         return res.status(400).json({ err: 'Email parameter missing.' });
-    if (!confirmNumber)
+    if (!code)
         return res.status(400).json({ err: 'ConfirmNumber parameter missing.' });
 
+    const msg = {
+        to: email,
+        from: EMAIL_FROM_NEWSLETTER,
+        subject: SUBJECT_CONFIRM,
+        html: HTML_CONFIRM
+    }
+
     try {
-        const contact = await getContactByEmail(email);
-        if (contact === null)
+        const contact = await Newsletter.findOne({ where: { email } });
+        if (!contact)
             return res.status(400).json({ err: 'Contact not found.' });
 
-        if (contact.custom_fields.confirmNumber === parseInt(confirmNumber)) {
-            const listID = await getListID(NAME_NEWSLETTER);
-            await addContactToList(email, listID);
+        if (contact.confirm)
+            return res.status(400).json({ err: 'Contact already subscribed.' });
+
+        if (contact.code === code) {
+            await sgMail.send(msg);
+            await Newsletter.update({ confirm: true }, { where: { email } });
+            res.json({ msg: 'Added successfully.' });
         }
         else {
             return res.status(400).json({ err: 'Confirmation number does not match' });
         }
-
-        const msg = {
-            to: email,
-            from: EMAIL_FROM_NEWSLETTER,
-            subject: SUBJECT_CONFIRM,
-            html: HTML_CONFIRM
-        }
-        await sgMail.send(msg);
-
-        res.json({ msg: 'Added successfully.' });
     }
     catch (error) {
         console.error(error.mesaage);
@@ -89,29 +85,27 @@ router.post('/confirm', async (req, res) => {
 });
 
 router.post('/unsubscribe', async (req, res) => {
-    const { email, confirmNumber } = req.body;
+    const { email, code } = req.body;
     if (!email)
         return res.status(400).json({ err: 'Email parameter missing.' });
-    if (!confirmNumber)
+    if (!code)
         return res.status(400).json({ err: 'ConfirmNumber parameter missing.' });
 
+    const msg = {
+        to: email,
+        from: EMAIL_FROM_NEWSLETTER,
+        subject: SUBJECT_UNSUBSCRIBE,
+        html: HTML_UNSUBSCRIBE
+    }
+
     try {
-        const contact = await getContactByEmail(email);
-        if (contact === null)
+        const contact = await Newsletter.findOne({ where: { email } });
+        if (!contact)
             return res.status(400).json({ err: 'Contact not found.' });
 
-        if (contact.custom_fields.confirmNumber === parseInt(confirmNumber)) {
-            const listID = await getListID(NAME_NEWSLETTER);
-            await deleteContactFromList(listID, contact);
-
-            const msg = {
-                to: email,
-                from: EMAIL_FROM_NEWSLETTER,
-                subject: SUBJECT_UNSUBSCRIBE,
-                html: HTML_UNSUBSCRIBE
-            }
+        if (contact.code === code) {
             await sgMail.send(msg);
-
+            await contact.destroy();
             res.json({ msg: 'You have been successfully unsubscribed.' });
         }
         else
@@ -131,37 +125,35 @@ router.post('/sendmail', async (req, res) => {
         return res.status(400).json({ err: 'Subject parameter missing.' });
 
     try {
-        // const listId = await getListID(NAME_NEWSLETTER);
-        // const listConstacs = await getContactsFromList(listId);
         let personalizations = [
             {
                 to: [
-                {
-                  email: "ferb@e.email"
+                    {
+                        email: "ferb@e.email"
+                    }
+                ],
+                dynamic_template_data: {
+                    textBody: text,
+                    unsubscribeLink: "ttp://localhost:3001"
                 }
-              ],
-              dynamic_template_data: {
-                textBody: text,
-                unsubscribeLink: "ttp://localhost:3001"
-              }
             },
-                        {
-              to: [
-                {
-                  email: "ferb@autistici.org"
+            {
+                to: [
+                    {
+                        email: "ferb@autistici.org"
+                    }
+                ],
+                dynamic_template_data: {
+                    textBody: text,
+                    unsubscribeLink: "http://localhost:3000"
                 }
-              ],
-              dynamic_template_data: {
-                textBody: text,
-                unsubscribeLink: "http://localhost:3000"
-              }
             }
-          ];
+        ];
 
         const msg = {
             personalizations,
             template_id: "d-6f40d484fddb4b98966a70d03ea3a122",
-            from: {email: EMAIL_FROM_NEWSLETTER},
+            from: { email: EMAIL_FROM_NEWSLETTER },
             subject: subject
         }
 
