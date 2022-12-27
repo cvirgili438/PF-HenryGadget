671f2767628325580e0fe730d435792b2c497afb
FerB <ferb@e.email>
2022-12-27 08:42:17 -0300

    Se corrige constantes.

--- api/src/routes/emails/constants/dataToSendMail.js
+++ api/src/routes/emails/constants/dataToSendMail.js
@@ -1,9 +1,10 @@
-const EMAIL_FROM_NEWSLETTER = 'proyectofinalhenrygadget@gmail.com';
+const EMAIL_FROM_NEWSLETTER = { email: 'proyectofinalhenrygadget@gmail.com', name: 'HenryGadget' };
 const PERSONALIZATION_ID = "d-6f40d484fddb4b98966a70d03ea3a122";
 
 const SUBJECT_SUBSCRIBE = 'HenryGadget: Confirm your subscription to our newsletter';
 const SUBJECT_CONFIRM = 'HenryGadget: Subscription succeed.';
 const SUBJECT_UNSUBSCRIBE = 'HenryGadget: Unsubscribe succeed.';
+const SUBJECT_SENDMAIL = 'HenryGadget: ';
 
 function htmlSubscribe(confirmationURL) {
     return `Hello subscripter, Thank you for subscribing to our newsletter. Please complete and confirm your subscription by <a href="${confirmationURL}"> clicking here</a>.`
@@ -15,6 +16,7 @@ module.exports = {
     SUBJECT_SUBSCRIBE,
     SUBJECT_CONFIRM,
     SUBJECT_UNSUBSCRIBE,
+    SUBJECT_SENDMAIL,
     htmlSubscribe,
     HTML_CONFIRM,
     HTML_UNSUBSCRIBE,
