e659c135700e7e43405f1e967104257e590ec940
ferb0 <ferb@e.email>
2022-12-26 21:23:35 -0300

    Se agrega constante.

--- api/src/routes/emails/constants/dataToSendMail.js
+++ api/src/routes/emails/constants/dataToSendMail.js
@@ -1,5 +1,5 @@
 const EMAIL_FROM_NEWSLETTER = 'proyectofinalhenrygadget@gmail.com';
-const NAME_NEWSLETTER = 'NewsLetter-HenryGadget';
+const PERSONALIZATION_ID = "d-6f40d484fddb4b98966a70d03ea3a122";
 
 const SUBJECT_SUBSCRIBE = 'HenryGadget: Confirm your subscription to our newsletter';
 const SUBJECT_CONFIRM = 'HenryGadget: Subscription succeed.';
@@ -12,11 +12,11 @@ const HTML_CONFIRM = 'Thank you for subscribing to our newsletter.';
 const HTML_UNSUBSCRIBE = 'I\'m sorry you have to go. Thanks for being with us. You can subscribe anytime you want.';
 module.exports = {
     EMAIL_FROM_NEWSLETTER,
-    NAME_NEWSLETTER,
     SUBJECT_SUBSCRIBE,
     SUBJECT_CONFIRM,
     SUBJECT_UNSUBSCRIBE,
     htmlSubscribe,
     HTML_CONFIRM,
-    HTML_UNSUBSCRIBE
+    HTML_UNSUBSCRIBE,
+    PERSONALIZATION_ID
 };
