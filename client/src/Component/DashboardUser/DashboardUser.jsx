8d06f7ea03463fca4a26c15499205ee7b70c6389
cvirgili438 <cvirgili438@gmail.com>
2023-01-05 18:25:31 -0300

    se corrigió el posteo de address

--- client/src/Component/DashboardUser/DashboardUser.jsx
+++ client/src/Component/DashboardUser/DashboardUser.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import {useSelector} from 'react-redux'
 
 export default function DashboardUser() {
+
+  const [token,setToken]=useState('')
+  const user = useSelector(state=>state.user)
+  const auth = getAuth()
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    onAuthStateChanged(auth, (user) => {
+  if (user) {
+    user.getIdToken().then((result) => {
+      setToken(result);
+    });
+  }
+});   
+}, [])
 
 
     
--- client/src/Redux/Actions/checkout.js
+++ client/src/Redux/Actions/checkout.js
@@ -11,10 +11,16 @@ export const setLocalAdress = (payload)=>(dispatch)=>{
 
 export const setAddress = (payload)=>(dispatch)=>{
     try {
-        let json = axios.post(`${URL}/address`,payload)
+        //let json = axios.post(`${URL}/address`,payload)
+        let json = axios({
+            url : `${URL}/address`,
+            method: 'post',
+            headers: {"Authorization":"Bearer " + payload.token},
+            data : payload
+        })
         return dispatch({
             type:SET_USER_ADDRESS,
-            payload:payload.result
+            payload:payload.data.result
         })
     }
     catch(error){console.log(error)}
