b620904c81f4df42fd6c48a900472b1cb8ae6170
Nicolas Stabilini <stabilini@gmail.com>
2023-01-06 13:12:10 -0300

    arreglo detalle menor que rompia

--- api/src/controller/objectToAdd/users.js
+++ api/src/controller/objectToAdd/users.js
@@ -1,12 +1,10 @@
 module.exports = [
     // proyectofinalhenrygadget@gmail.com
     { uid: 'ZTAC4VigPiX2a2VWQM939zMdSaF2', rol: 'admin' },
-    // pruebafer@fer.com
-    { uid: 'hr22OxMDfIc8aUkS9YYXXJ1jG4r2', rol: 'client' },
     // varios tests
-    { uid: 'few4ferf4faewf4aefawefa4af4f', rol: 'client' },
-    { uid: 'adfasdfdsfsfsa4r4r44r61gerqg', rol: 'client' },
-    { uid: 'a34t2q343grgsergffsfgzcxsxxs', rol: 'client' },
-    { uid: 'qq3fq32rt34tq4taet44ta4ta4at', rol: 'client' },
-    { uid: '32443t4a4t4t34t343t334ta34tt', rol: 'client' },
+    { uid: 'hr22OxMDfIc8aUkS9YYXXJ1jG4r2', rol: 'client', displayName: 'Pablo Lampone', email: 'basilio@simulad.or', photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3uIVDhybW_4vcn-NiCU7_HZgL7Ia-oa6hDVnkh_xP6hIwSankgYLyHd7y1BGWhk14ewM&usqp=CAU'},
+    { uid: 'few4ferf4faewf4aefawefa4af4f', rol: 'client', displayName: 'Emilio Ravenna', email: 'camaleon@simulad.or', photoURL: 'https://img.ecartelera.com/personas/mini/3900/3988_th.jpg' },
+    { uid: 'adfasdfdsfsfsa4r4r44r61gerqg', rol: 'client', displayName: 'Mario Santos', email: 'habano@simulad.or', photoURL: 'https://robertoramasso.com/contrataciones/contratar-a-federico-delia.jpg' },
+    { uid: 'a34t2q343grgsergffsfgzcxsxxs', rol: 'client', displayName: 'Gabriel Medina', email: 'dupont@simulad.or', photoURL: 'https://www.clarin.com/img/2019/05/08/xfM3qD2qV_360x240__1.jpg' },
+    { uid: '22eqdsd23rffefwefw3fwefdsdf3', rol: 'client', displayName: 'Sin foto ni mail' },
 ];
