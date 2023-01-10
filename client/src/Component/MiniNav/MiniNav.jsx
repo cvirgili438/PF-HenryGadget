59fbf8d1dec4014dd17c01afd20df441b07d313d
Leonardo Meza <leoariel17@gmail.com>
2023-01-10 15:57:13 -0300

    se modifico un poco la navbar y los componentes que se veian afectados por ella

--- client/src/Component/MiniNav/MiniNav.jsx
+++ client/src/Component/MiniNav/MiniNav.jsx
@@ -1,22 +1,46 @@
 import React from "react"
 import { Link } from "react-router-dom"
-
+import ButtonBorderEffect from "../Buttons/ButtonBorderEffect/ButtonBorderEffect"
 import styles from './MiniNav.module.css'
 
-const MiniNav = () => {
+const MiniNav = ({ pathname }) => {
 
     return (
-        <nav>
-            <ul className={styles.miniNav}>
-                <Link to='/'>
-                    <li>Inicio</li>
-                </Link>
-
-                <Link to='/home'>
-                    <li>Products</li>
-                </Link>
-            </ul>
-        </nav>
+        <div className={styles.main}>
+            <Link to='/'>
+                <span className={styles.separator}>
+                    <ButtonBorderEffect text='Home' />
+                </span>
+            </Link>
+            <Link to='/products'>
+                <span className={styles.separator}>
+                    <ButtonBorderEffect text='Store' />
+                </span>
+            </Link>
+            {pathname === '/'
+                ? (
+                    <>
+                        <a href="#anchor-services">
+                            <span className={styles.separator}>
+                                <ButtonBorderEffect text='Our services' />
+                            </span>
+                        </a>
+                        <a href='#anchor-featured'>
+                            <span className={styles.separator}>
+                                <ButtonBorderEffect text='Featured products' />
+                            </span>
+                        </a>
+                        <a href='#anchor-about'>
+                            <span className={styles.separator}>
+                                <ButtonBorderEffect text='About us' />
+                            </span>
+                        </a>
+                    </>
+                )
+                :
+                null
+            }
+        </div>
 
     )
 }
