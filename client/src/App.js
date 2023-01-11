bc6f28beea1d9f8439b59e00c7a4ffb5ab6cc500
cvirgili438 <100769556+cvirgili438@users.noreply.github.com>
2023-01-11 20:29:15 -0300

    bug user

--- client/src/App.js
+++ client/src/App.js
@@ -50,9 +50,9 @@ const App = () => {
           <Route  path="/review/:id" component={Review}  />  
           <Route path={'/map'} component={Locations} /> 
           
-          {user && user.rol === 'client' && (
+          {(user && user.rol === 'client') ? (
             <Route  path={"/user"}  component={DashboardUser}  />
-            )
+            ): (<Route path="/user" component={Page404} />)
           }
           <Route path='/products' component={Products}/>
 
