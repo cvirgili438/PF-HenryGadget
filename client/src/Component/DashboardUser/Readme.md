7c082eefbcf7e0606dc18c69dbe5ac21f97a167f
Carlos <cvirgili438@gmail.com>
2023-01-11 16:54:06 -0300

    readme y limpie un poco de codigo

---
+++ client/src/Component/DashboardUser/Readme.md
@@ -0,0 +1,17 @@
+1 COMPONENTE DashboardUser)Bueno, la cosa es asi,en la carpeta Principal tienen el inicio del flow, aca se obtiene el token
+y se obtiene los estados de address 
+se actualizan los estados de los usuarios forzozamente porque la pagina no lo hace bien( otros componentes actualizan mal los estados de usuarios asi que lo force a que lo actualice bien )
+luego a las tablas se le pasa como props el token, las address del usuario y las propiedades del usuario tambien
+2 BasicTabs/ Tabs.jsx) el flow sigue en el componente Tabs al cual se le paso las propiedades del componente DashboardUser.jsx del principal
+Como primera instancia, se setea un Modal, que se abre cuando se hace click en un boton " Create New Address" 
+en este componente Tabs, tambien se divide en 2 partes, una tabla adecuada a Profile y otra a Addresses.
+la tabla  addresses comenzara en la linea de codigo 101
+3Addresses)Aqui, si el usuario no tiene ninguna Address ligada a la cuenta solo renderisa el boton, pero si tiene una Address o mas, ligada a la cuenta Renderisa el componente o los componentes, Addresses en la lina de codigo 110 
+Aqui se usa un componente de Material UI para dividir el titulo ( nombre de la Address) y el Componente AddressForm el cual es el formulario.
+4 AddressForm)A este formulario se le pasa como Props los estados del Switch de Material UI, para activar o desactivar la propiedad "disabled" para poder editar o no la address.
+Aqui adentro esta los dispatch de los TextField
+
+-------------------------------------------------------------------
+En el Tabs/BasicTabs, sigue la parte de la tabla de Profile, 
+Aqui se renderisa el componente ProfileForm, con un ternario, si es editable o no 
+Y luego aqui se renderisa un ProfileFormGoogle o UploadImage Dependiendo si el usuario esta logueado con cuenta de 3ro ( google) o solamente con la DB 
--- client/src/Component/DashboardUser/Secondary/ProfileFormGoogle.jsx
+++ client/src/Component/DashboardUser/Secondary/ProfileFormGoogle.jsx
@@ -10,92 +10,17 @@ import UploadImageGoogle from './UploadImageGoogle';
 
 
 export default function ProfileFormGoogle(props) {
-    let errors = {}
-    function error (input){
-        var numberRegex = /^\d+$/;
-        if(!numberRegex.test(input)){
-            errors.phoneNumber = false    }
-        return numberRegex.test(input);
-    }
-    function disabledButton(errors){
-        if(errors.phoneNumber === false){
-            return true
-        }
-        else return false
-    }
+   
     
-    const dispatch=useDispatch()   
-    const [state, setState] = React.useState({phoneNumber: '', photoURL : ''});
-    
- 
-
-    const handleChange = (event) => {        
-        setState({
-        ...state,
-        [event.target.name]: event.target.value,
-        });        
-        
-    };
-    function onClick(e){
-      e.preventDefault();
-      if(e.target.disabled === true){
-        return
-      }
-      if(e.target.disabled === false){
-        e.target.value= ''
-      }
-    }
-    const phone = document.getElementsByName('phoneNumber')
-  
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // submit the form
-        if(state.photoURL === '' && props.user.photoURL !== '' ){setState({...state,photoURL:props.user.photoURL})}
-       dispatch(putProfileUser({
-        google:true,
-        idUser:props.user.uid,
-        phoneNumber:state.phoneNumber,
-        photoURL:state.photoURL}))               
-    };
-
   return (
-   <div className={styles.divCenter}>    
-     
-      {/* <TextField        
-        error={!error(state.phoneNumber)}
-        helperText={'Without spaces'}
-        label="phoneNumber"
-        name="phoneNumber"
-        defaultValue={props.user.phoneNumber || 'Please fill this field' }
-        onClick={e => onClick(e)}
-        variant="standard"
-        disabled={props.disabled}
-        onChange={handleChange}
-      /> */}
-      {/* <TextField
-        label="photoURL"
-        name="photoURL"
-        defaultValue={props.user.photoURL || 'Please fill this field'  }
-        onClick={e => onClick(e)}
-        variant="standard"
-        disabled={props.disabled}
-        onChange={handleChange}
-      />    */}
+   <div className={styles.divCenter}>        
       <UploadImageGoogle
       currentPhoto={props.user.photoURL ? props.user.photoURL : ''}
       google={true} 
       disabled={props.disabled}
       phoneNumber={props.user.phoneNumber}
        idUser={props.user.uid} />
-        {/* <Button onClick={handleSubmit} variant="contained" 
-        disabled={disabledButton(errors)}
-        sx={{
-          backgroundColor: 'black',
-          color:'white'
-          }}
-          color="primary">
-      Submit
-    </Button> */}
+     
     </div>
   )
 }
