04e5b9d625eb1babcb22d6d959bd315035f3c7f9
unknown <svenandreasclausz@gmail.com>
2023-01-12 20:45:02 -0300

    Slider

--- client/src/Component/Slider/Slider.jsx
+++ client/src/Component/Slider/Slider.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel'
-
 import styles from './Slider.module.css'
 import prueba from '../../Assets/prueba.jpg'
 import {Button} from '@mui/material'
@@ -24,11 +23,7 @@ export default function Slider() {
                 style={{objectFit:'contain'}}
               />
             </div>
-    
-            {/* <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption> */}
+  
           </Carousel.Item>
           <Carousel.Item>
             <div style={{width:'100%',backgroundColor:'rgb(237,237,237)'}}>
@@ -40,46 +35,16 @@ export default function Slider() {
               />
 
             </div>
-            {/* <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption> */}
-          </Carousel.Item>
-          {/* <Carousel.Item>
-            <div style={{width:'100%',backgroundColor:'#19b2a6',positon:'relative'}}>
-            <Link to='/products'>
-              <Button variant='contained' color='primary' sx={{
-                zIndex:'1',marginTop:'10rem',position:'absolute',border:'none',fontSize:'2.3rem',borderRadius:'10px'
-              }}>Buy now</Button>
-            </Link>
+           </Carousel.Item>
+        
+            <Carousel.Item>
               <img
                 className="d-block w-100"
-                src={prueba}
-                alt="Second slide"
-                style={{objectFit:'contain'}}
+                src="https://zelucashdev.s3.sa-east-1.amazonaws.com/uploads/BANERS%20ZC/09_Septiembre/Samsung%20Linea%20A%20-%20Linea%20J.png"
+                alt="Third slide"
               />
-            </div> */}
-    
-            {/* <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption> */}
-          {/* </Carousel.Item> */}
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://zelucashdev.s3.sa-east-1.amazonaws.com/uploads/BANERS%20ZC/09_Septiembre/Samsung%20Linea%20A%20-%20Linea%20J.png"
-              alt="Third slide"
-            />
-    
-            {/* <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption> */}
-          </Carousel.Item>
-        </Carousel>
+            </Carousel.Item>
+          </Carousel>
         </div>
       );
     }
