ed911b5529ea7b1aad159016823d90debb335483
unknown <svenandreasclausz@gmail.com>
2023-01-12 21:34:53 -0300

    Agrega un slider, retoq miniNavaBar en landing, acomodé %tajes se veian la caja muy grande y el texto arriba

--- client/src/Component/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/Component/FeaturedProducts/FeaturedProducts.jsx
@@ -4,7 +4,7 @@ import 'aos/dist/aos.css'
 import {Typography } from '@mui/material'
 function FeaturedProducts() {
   return (
-    <div  id='anchor-featured' data-aos='flip-down' data-aos-offset='-200' data-aos-duration='1300' data-aos-once='true'  style={{maxWidth:'100%',height:'auto',backgroundColor:'rgb(242,242,242)',paddingBottom: '2rem',marginBottom:'8rem'}}>
+    <div  id='anchor-featured' data-aos='flip-down' data-aos-offset='-200' data-aos-duration='1300' data-aos-once='true'  style={{maxWidth:'100%',height:'auto',backgroundColor:'rgb(242,242,242)',paddingBottom: '2rem',marginBottom:'8rem',paddingTop:'3rem'}}>
       {/* <h1 style={{margin:'2.5rem',fontSize:'5rem'}}>Feautered Products</h1> */}
       <Typography variant="h3" sx={{marginTop:'3.5rem'}} gutterBottom>
         Featured products
